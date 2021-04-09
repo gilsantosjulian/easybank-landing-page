21fefcb473e77d40bb44e231e88785404cbc2ede
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2021-04-08 22:53:41 -0500

    feat: add javasctipt animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log('Hello Word');
+const btnHamburguer = document.querySelector('#btnHamburguer')
+
+btnHamburguer.addEventListener('click', handleChange)
+
+function handleChange () {
+  console.log('open hamburguer');
+  if(btnHamburguer.classList.contains('open'))
+    btnHamburguer.classList.remove('open')
+  else 
+    btnHamburguer.classList.add('open')
+}
