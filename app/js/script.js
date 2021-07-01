b6ab23e55e0e0e40fee1185a37d67f07078e8a37
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2021-06-30 20:58:30 -0500

    feat: update script with overlay beviour

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,19 @@
 const btnHamburguer = document.querySelector('#btnHamburguer')
 const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
 
 btnHamburguer.addEventListener('click', handleChange)
 
 function handleChange () {
   console.log('open hamburguer');
-  if(header.classList.contains('open'))
+  if(header.classList.contains('open')) { // Close hamburguer menu
     header.classList.remove('open')
-  else 
+    overlay.classList.remove('fade-in')
+    overlay.classList.add('fade-out')
+  }
+  else { // Open hamburguer menu
     header.classList.add('open')
+    overlay.classList.remove('fade-out')
+    overlay.classList.add('fade-in')
+  }
 }
